7328f353086c27c050569f6ed5b9bf1c9de4f987
Priyanshu Rajput <priyanshu.rajput_cs19@gla.ac.in>
2022-12-01 17:56:51 +0530

    Did some changes in dialog box

--- src/Components/Popup.js
+++ src/Components/Popup.js
@@ -5,10 +5,8 @@ import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
     padding: theme.spacing(2),
@@ -17,7 +15,7 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     padding: theme.spacing(1),
   },
 }));
-function BootstrapDialogTitle(props) {
+function dialogTitle(props) {
   const { children, onClose, ...other } = props;
   return (
     <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
@@ -39,7 +37,7 @@ function BootstrapDialogTitle(props) {
     </DialogTitle>
   );
 }
-BootstrapDialogTitle.propTypes = {
+dialogTitle.propTypes = {
   children: PropTypes.node,
   onClose: PropTypes.func.isRequired,
 };
@@ -57,13 +55,12 @@ export default function CustomizedDialogs({children,namee,title}) {
        {namee}
       </Button>
       <BootstrapDialog
-        // onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
           {title}
-        </BootstrapDialogTitle>
+        </DialogTitle>
         <DialogContent dividers>
           {children}
         </DialogContent>
